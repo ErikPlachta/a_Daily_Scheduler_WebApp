6f7752141839941ebf2c9c5a4341b1199456d07c
Erik Plachta <eplachta@gmail.com>
2021-11-29 21:38:35 -0500

    framed in source. Ready to start building

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,44 @@
+/* -------------------------------------------------------------------------- */
+//-- GLOBALS -> START
 
+// DIV containin all times
+const schedule_Today = $("#schedule_Today");
 
+//-- GLOBALS -> END
 /* -------------------------------------------------------------------------- */
 //-- DATABASE -> START
 
+/* --------------------------
+//-- get_DATABASE() -> START
 
+
+
+//-- get_DATABASE() -> END
+/* --------------------------
+//-- set_DATABASE() -> START
+
+
+
+//-- set_DATABASE() -> END
+/* -----------------------
 //-- DATABASE -> END
 /* -------------------------------------------------------------------------- */
 //-- SCHEDULER -> START
 
 function build_Schedule(){
+    /* Builds the calendar */
+    
     console.log("function build_Schedule()");
-}
 
+    // in military time //
+    let hours_Day = 24;
+    // start of business hours. 7am //
+    let hours_Start = 7;
+    // end of business hours. 6 pm  //
+    let hours_End = 18;
+
+    
+};
 
 
 

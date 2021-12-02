369a62fb75f0eede8563a4f6b2afc3bf9fb06e68
Erik Plachta <eplachta@gmail.com>
2021-12-01 19:15:55 -0500

    i14 - framed in calendar. making branches now that I've got a plan

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,28 @@ function build_Schedule(){
     let hours_End = 18;
 
     //-- Building HTML
+    
+    // Grabbing div
+    let schedule_Today = document.getElementById("schedule_Today");
+    
+    // Make sure there is nothing in list from previous questions
+    schedule_Today.innerHTML="";
+
+    for(let i =0; i < hours_Day; i++) {
+       
+        console.log(i)
+
+        var div = document.createElement("div");
+        div.innerText = i;
+        div.setAttribute("id","hour_"+i);
+        div.setAttribute("class","hour");
+        
+        schedule_Today.appendChild(div);
+    };
+
 };
 
+build_Schedule();
     
 //-- SCHEDULER -> END
 /* -------------------------------------------------------------------------- */
@@ -39,7 +59,7 @@ function get_Database(){
 
     // Get Database
     let database_Current = JSON.parse(localStorage.getItem(database_Name));
-    
+    console.log(database_Current);
     // If database exists
     if (database_Current != null) {
         
@@ -147,10 +167,12 @@ console.log("//-- script.js Running set_Database()...")
 set_Database();
 
 function testing_BuildDatabase() {
-
+    
     // fully fleshed out all hours for just 1 day. 
         // TODO:: 12/01/2021 #EP || This best way to do it?
-    let daily = {
+    
+    let a_DailyScheduler_demo = {
+        daily: {
         '12/01/2021' : {
             '00:00' : {
                 state: 0, 
@@ -253,24 +275,28 @@ function testing_BuildDatabase() {
                 description: null
             },
         }
+        },
+
+        settings: {
+           defaults: {
+               timeFormat: "12", // 12 or 24
+
+               // Business Hours - in military time for settings
+               dayStart: "08:00", 
+               dayEnd: "18:00"
+           },
+
+           // If user defines these settings, will over-ride defaults
+           user: {
+               timeFormat: null,
+               dayStart: null,
+               dayEnd: null,
+           }
+        }
     };
 
-    let settings = {
-        defaults: {
-            timeFormat: "12", // 12 or 24
-            
-            // Business Hours - in military time for settings
-            dayStart: "08:00", 
-            dayEnd: "18:00"
-        },
-        
-        // If user defines these settings, will over-ride defaults
-        user: {
-            timeFormat: null,
-            dayStart: null,
-            dayEnd: null,
-        }
-    }
-
     //TODO:: 12/01/2021 #EP || Add build database stuff here
+
+    localStorage.setItem("a_DailyScheduler",JSON.stringify(a_DailyScheduler_demo));
 };
+// testing_BuildDatabase();
